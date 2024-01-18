ec75f5e4ff85c1f08bab932e2133f1f6e2851640
Charles Langlois <contact@charleslanglois.dev>
2024-01-17 19:26:23 -0500

    Updated push.rsync.sh

--- push.rsync.sh
+++ push.rsync.sh
@@ -1,1 +1,3 @@
-rsync -avzP -e ssh ./public/ rsync://root@viachicago.vultr/charleslanglois.dev/public
+HOST=${HOST:-${1:-pursuit}}
+PATH=www.charleslanglois.dev
+rsync -avzP -e 'ssh -F hosts/ssh_config' ./public/ rsync://$HOST/$PATH
