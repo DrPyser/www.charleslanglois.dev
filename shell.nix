37755b532e1f42c02e5087747b7527dcf9fc7c93
Charles Langlois <contact@charleslanglois.dev>
2024-01-22 20:26:45 -0500

    shell.nix: refactored & formatted

--- shell.nix
+++ shell.nix
@@ -1,27 +1,19 @@
-{ pkgs ? import <nixpkgs> {}}:
-let
-  theme = import ./theme.nix {inherit pkgs;};
-in
+{ pkgs ? import <nixpkgs> { }, theme ? import ./theme.nix { inherit pkgs; } }:
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    hugo
-    ansible
-    flyctl
-  ];
+  buildInputs = with pkgs; [ hugo ansible flyctl nixfmt ];
   shellHook = ''
-  # set -x
-  echo "Entering development environment for $PWD"
-  echo "hugo=$(realpath $(which hugo))"
-  echo "ansible=$(realpath $(which ansible))"
-  echo "theme=${theme.name}@${theme.path}"
-  export THEME=${theme.name}
+    # set -x
+    echo "Entering development environment for $PWD"
+    echo "hugo=$(realpath $(which hugo))"
+    echo "ansible=$(realpath $(which ansible))"
+    echo "theme=${theme.name}@${theme.path}"
+    export THEME=${theme.name}
 
-  if ! test -d ./themes/$THEME; then
-    ln -sf ${theme.path} themes/$THEME;
-  fi
-  echo "Theme $THEME available at $(ls -d ./themes/$THEME)";
-  export PS1="[$(nix-shell-info)] $PS1"
+    if ! test -d ./themes/$THEME; then
+      ln -sf ${theme.path} themes/$THEME;
+    fi
+    echo "Theme $THEME available at $(ls -d ./themes/$THEME)";
+    export PS1="[$(nix-shell-info)] $PS1"
   '';
 }
-
 
